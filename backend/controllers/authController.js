c13c4e93cb928908f1b78e658391c6c13c6f97aa
Chirel <chirelhalioua@gmail.com>
2025-01-14 16:21:03 +0100

    Correction de l'import bcrypt

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,6 @@
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const validator = require('validator');
 
 // Inscription d'un nouvel utilisateur
