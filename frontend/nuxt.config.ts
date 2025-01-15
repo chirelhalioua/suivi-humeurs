63c9952ad397b243556254e902d361476b5eaa33
ChirelHalioua <72791846+chirelhalioua@users.noreply.github.com>
2025-01-15 14:49:57 +0100

    Update nuxt.config.ts

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,9 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@pinia/nuxt'],
+  plugins: [
+    '~/plugins/axios.js',
+  ],
   app: {
     head: {
       title: 'Les Humeurs à la Funès',
